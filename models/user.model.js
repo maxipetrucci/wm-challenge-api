85c6d17947e29788cc042c01571e657249ba96e8
Maximiliano Petrucci <maxipetrucci>
2021-07-06 11:47:47 -0300

    Agregada la imagen del usuario

--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,7 @@ const User = DatabaseService.get().define('users', {
     id: { type: Sequelize.BIGINT.UNSIGNED, primaryKey: true },
     username: Sequelize.STRING,
     name: Sequelize.STRING,
+    image_url: Sequelize.STRING,
     verified: Sequelize.BOOLEAN
 }, { timestamps: false });
 
