8ef1b9191e1b29aaf361173bf2423151efb5955a
Maximiliano Petrucci <maxipetrucci>
2021-07-07 12:46:15 -0300

    Corregidos los parametros de la api

--- services/twitter.service.js
+++ services/twitter.service.js
@@ -4,26 +4,27 @@ const twitterClient = new Twitter({
     bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
 
-const searchLastSevenDaysJavascriptTweets = async (params = {}) => {
+const searchLastSevenDaysJavascriptTweets = async ({ fromId, maxResults }) => {
     const DEFAULT_MAX_RESULTS = 25;
     const DAYS_TO_SUBSTRACT = 7;
     
-    var startDate = new Date();
-    startDate.setDate(startDate.getDate() - DAYS_TO_SUBSTRACT);
-    
-    var searchParams = {
+    let searchParams = {
         query: "javascript",
-        max_results: params.maxResults || DEFAULT_MAX_RESULTS,
-        start_time: startDate.toISOString(),
+        max_results: maxResults || DEFAULT_MAX_RESULTS,
         "tweet.fields": "attachments,author_id,created_at,public_metrics,in_reply_to_user_id",
         "user.fields": "name,profile_image_url,username,verified",
         expansions: "author_id"
     };
-
-    if (params.fromId) {
-        searchParams.since_id = params.fromId;
-    }
     
+    if (fromId) {
+        searchParams.until_id = fromId;
+    }
+    else {
+        var startDate = new Date();
+        startDate.setDate(startDate.getDate() - DAYS_TO_SUBSTRACT);
+        searchParams.start_time = startDate.toISOString();
+    }
+
     return twitterClient.get('tweets/search/recent', searchParams);
 }
 
