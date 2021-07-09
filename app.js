4e758b3710e53a06e01474f2aa5a538423d638c2
Maximiliano Petrucci <maxipetrucci>
2021-07-09 17:43:33 -0300

    Agregado CORS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const DatabaseService = require('./services/database.service');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
